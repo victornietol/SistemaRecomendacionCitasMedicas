b841a2d892d7a80c1d165a2116a7347e2bec1c56
Daniel Angeles <127440200+Danno98@users.noreply.github.com>
2024-05-05 22:00:11 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Recomendacion citas medicas</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="css.css">
+</head>
+<body>
+        <div class="encabezado">
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+          <div class="container-fluid">
+              <a class="navbar-brand" href="index.php">
+                  <img src="cruz.png" alt="..." width="30" height="24">
+              </a>
+              <a class="nav-link" href="index.php">Home</a>
+                      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                      </button>
+                  <div class="collapse navbar-collapse" id="navbarNav">
+                          <ul class="navbar-nav">
+                          <li class="nav-item">
+                          <a class="nav-link" aria-current="page" href="form.php">Sintomas</a>
+                       </li>
+                       <li class="nav-item">
+              <a class="nav-link" href="Doctores.php">Doctores</a>
+                       </li>
+                       <li class="nav-item"></li>
+          </div>
+          </div>
+        </nav>
+      </div>
+      <div class="titulo">
+        <h1>Encuentra al especialista adecuado para ti</h1>
+      </div>
+      <div class="img1">
+        <img src="doc.jpg" class="rounded mx-auto d-block" alt="..." width="500px" height="500px">
+      </div>
+      <div class="desc">
+        <p>Contesta el cuestionario con los sintomas que presentas y en seguida nuestro sistema experto te recomendará al especialista que necesitas</p>
+        <p>En la barra de navegacion esta la pestaña de Doctores en donde encontrarás el contacto de cada uno </p>
+        <p>Contamos con doctores muy bien calificados con los cuales puedes agendar tu cita</p>
+      </div>
+</body>
